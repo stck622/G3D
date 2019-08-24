aa2a39baf9715a6793a7085d074627b4643be304
stck622 <stck622@naver.com>
2019-08-24 12:13:25 +0900

    FIRMWARE, LCD 솔루션 추가

---
+++ FIRMWARE/FIRMWARE.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		FIRMWARE.ino
+ Created:	2019-08-24 오후 12:11:14
+ Author:	stck6
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
