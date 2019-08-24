22797012df7af4132275af1e34dcac85d713715b
Han ji wan <42163754+aufmcl@users.noreply.github.com>
2019-08-24 17:01:51 +0900

    SD추가

--- G3D.Core.SD/__vm/.G3D.Core.SD.vsarduino.h
+++ G3D.Core.SD/__vm/.G3D.Core.SD.vsarduino.h
@@ -78,6 +78,6 @@ typedef void *__builtin_va_list;
 #define pgm_read_float(address_short) float()
 #define pgm_read_ptr(address_short)   short()
 
-#include "G3D.FIRMWARE.ino"
+#include "G3D.Core.SD.ino"
 #endif
 #endif
