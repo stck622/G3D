f59dd2301c5c00f14e02b2bc8859ce245121619a
stck622 <stck622@naver.com>
2019-08-27 17:00:46 +0900

    파일 정리

---
+++ G3D.FIRMWARE/Termister.cpp
@@ -0,0 +1,7 @@
+// 
+// 
+// 
+
+#include "Termister.h"
+
+
---
+++ G3D.FIRMWARE/Termister.h
@@ -0,0 +1,14 @@
+// Termister.h
+
+#ifndef _TERMISTER_h
+#define _TERMISTER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
