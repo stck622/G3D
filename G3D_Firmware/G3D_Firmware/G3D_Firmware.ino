12776c11aaf140d5ee8d2608727b58a321a54637
stck622 <stck622@naver.com>
2019-10-10 15:58:52 +0900

    Firmware create

---
+++ G3D_Firmware/G3D_Firmware/G3D_Firmware.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		G3D_Firmware.ino
+ Created:	2019-10-10 오후 3:58:15
+ Author:	KWB
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
