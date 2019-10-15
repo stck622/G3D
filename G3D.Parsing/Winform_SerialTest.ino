6e30b76da7a23c470aafc71f1cc09ab926fa0980
Han ji wan <42163754+aufmcl@users.noreply.github.com>
2019-10-15 22:25:51 +0900

    파서 수정
    값이 0만 뜨던 문제는 해결함
    하지만 값이 초반에만 잘 읽히다가
    점점 갈 수 록 값이 이상해짐

--- G3D.Parsing/Winform_SerialTest.ino
+++ G3D.Parsing/Winform_SerialTest.ino
@@ -8,8 +8,7 @@ double renew_xy_pos[][4] = { 0, };
 float x = 0, y = 0, e = 0;
 int f = 0, g = 0, cnt = 0;
 
-float x_float = 0, y_float = 0, e_float = 0;
-char f_char[10], g_char[5], x_char[10], y_char[10], e_char[10];
+
 int g_int, f_int, j = 0, k = 0;
 
 float atf(char buff[]);
@@ -19,6 +18,8 @@ char gcode[100];
 char buf[10] = {0, };
 
 void loop() {
+  float x_float = 0, y_float = 0, e_float = 0;
+  char f_char[10], g_char[5], x_char[10], y_char[10], e_char[10];
   if (Serial.available()) {
     /*
         if (Serial.find("G") || Serial.find("X") || Serial.find("Y") || Serial.find("E") || Serial.find("F")) {
@@ -40,10 +41,17 @@ void loop() {
             f = atoi(buf);
           else
             f = f;
-    */
 
+
+
+          cnt++;
+          j = 0; k = 0;
+          Serial.print("G = " + String(g) + ", X = " + String(x) + ", Y = " + String(y));
+          Serial.println(", E = " + String(e) + ", F = " + String(f) + ", CNT = " + String(cnt));
+        }
+    */
     //String gcode = Serial.readString();
-    leng = Serial.readBytes(gcode, 50);
+
     /*
         Serial.println(leng);
         for (int i = 0; i < leng; i++) {
@@ -52,89 +60,86 @@ void loop() {
         Serial.println();
     */
     //Serial.println(leng);
+    leng = Serial.readBytes(gcode, 50);
     for (int i = 0; i < leng; i++) {
-
       if (gcode[i] == 'G') {
-        j = i;
-        while (gcode[j] != ' ') {
-          j++;
+        for (int j = i + 1; j < leng; j++) {
 
-          g_char[k] = gcode[j];
-          k++;
+          if (gcode[j] == ' ')
+            break;
+          else {
+            g_char[k] = gcode[j];
+            k++;
+          }
         }
         k = 0;
-        g_int = atoi(g_char);
-        Serial.print("G = " + String(g_int));
       }
+
+
       if (gcode[i] == 'X') {
-        j = i;
-        while (gcode[j] != ' ')
-        {
-          j++;
+        for (int j = i + 1; j < leng; j++) {
 
-          x_char[k] = gcode[j];
-          k++;
+          if (gcode[j] == ' ')
+            break;
+          else {
+            x_char[k] = gcode[j];
+            k++;
+          }
         }
         k = 0;
-        x_float = atof(x_char);
-        Serial.print(", X = " + String(x_float));
       }
-      if (gcode[i] == 'Y') {
-        j = i;
-        while (gcode[j] != ' ') {
-          j++;
 
-          y_char[k] = gcode[j];
-          k++;
+      if (gcode[i] == 'Y') {
+        for (int j = i + 1; j < leng; j++) {
+
+          if (gcode[j] == ' ')
+            break;
+          else {
+            y_char[k] = gcode[j];
+            k++;
+          }
         }
         k = 0;
-        y_float = atof(y_char);
-        Serial.print(", Y = " + String(y_float));
       }
       if (gcode[i] == 'E') {
-        j = i;
-        while (gcode[j] != ' ') {
-          j++;
+        for (int j = i + 1; j < leng; j++) {
 
-          e_char[k] = gcode[j];
-          k++;
+          if (gcode[j] == ' ')
+            break;
+          else {
+            e_char[k] = gcode[j];
+            k++;
+          }
         }
         k = 0;
-        e_float = atof(e_char);
-        Serial.print(", E = " + String(e_float));
       }
       if (gcode[i] == 'F') {
-        j = i;
-        while (gcode[j] != '\0') {
-          j++;
+        for (int j = i + 1; j < leng; j++) {
 
-          f_char[k] = gcode[j];
-          k++;
+          if (gcode[j] == '\0')
+            break;
+          else {
+            f_char[k] = gcode[j];
+            k++;
+          }
         }
         k = 0;
-        f_int = atoi(f_char);
-        Serial.print(", F = " + String(f_int));
       }
     }
-    /*
-        Serial.print("G = " + String(g_char) + ", X = " + String(x_char) + ", Y = " + String(y_char));
-        Serial.println(", E = " + String(e_char) + ", F = " + String(f_char) + ", CNT = " + String(cnt));
-    */
 
 
-    //cnt++;
-    /*
-      Serial.println(g_int);
-      Serial.println(x_float);
-      Serial.println(y_float);
-      Serial.println(e_float);
-      Serial.println(f_int);
-    */
-    Serial.println(", CNT = " + String(cnt));
+    g_int = atoi(g_char);
+    x_float = atof(x_char);
+    y_float = atof(y_char);
+    e_float = atof(e_char);
+    f_int = atoi(f_char);
 
+
+    Serial.print("G = " + String(g_int) + ", X = " + String(x_float) + ", Y = " + String(y_float));
+    Serial.println(", E = " + String(e_float) + ", F = " + String(f_int) + ", CNT = " + String(cnt));
     cnt++;
-    j = 0; k = 0;
-    //Serial.print("G = " + String(g) + ", X = " + String(x) + ", Y = " + String(y));
-    //Serial.println(", E = " + String(e) + ", F = " + String(f) + ", CNT = " + String(cnt));
+
+
+    //Serial.println(", CNT = " + String(cnt));
   }
 }
