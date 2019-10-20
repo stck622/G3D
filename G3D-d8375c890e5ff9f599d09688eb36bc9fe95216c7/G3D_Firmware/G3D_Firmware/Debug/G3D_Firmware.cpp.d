d9b5fda37c8be6a6b397d9554729c8244c541c03
stck622 <stck622@naver.com>
2019-10-20 09:17:49 +0900

    re

---
+++ G3D-d8375c890e5ff9f599d09688eb36bc9fe95216c7/G3D_Firmware/G3D_Firmware/Debug/G3D_Firmware.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\G3D_Firmware.cpp.o: \
+ C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\G3D_Firmware.cpp \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\stck6\Desktop\arduino-1.8.10-windows\arduino-1.8.10\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\Status.h \
+ C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\StepMotor.h
---
+++ G3D-d8375c890e5ff9f599d09688eb36bc9fe95216c7/G3D_Firmware/G3D_Firmware/Debug/StepBuffer.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\StepBuffer.cpp.o: \
+ C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\StepBuffer.cpp \
+ C:\Users\stck6\AppData\Local\Temp\VMBuilds\G3D_Firmware\mega_atmega2560\Debug\StepBuffer.h
